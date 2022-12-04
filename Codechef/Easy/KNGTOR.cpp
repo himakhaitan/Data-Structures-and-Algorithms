5a49a81bd91d277e2b9f75ffb054c27c1fe42fe7
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-12-05 04:25:12 +0530

    Codechef Solved

---
+++ Codechef/Easy/KNGTOR.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int m, n;
+        cin >> n >> m;
+        cout << (n * 5) + (m * 7) << endl;
+    }
+
+    return 0;
+}
